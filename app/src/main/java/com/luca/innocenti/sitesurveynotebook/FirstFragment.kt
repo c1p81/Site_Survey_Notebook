7c163f9b54859ca8a7773bbbd46139c619a251bd
c1p81 <lucainnoc@gmail.com>
2020-04-22 18:34:41 +0200

    file tmp

--- app/src/main/java/com/luca/innocenti/sitesurveynotebook/FirstFragment.kt
+++ app/src/main/java/com/luca/innocenti/sitesurveynotebook/FirstFragment.kt
@@ -4,23 +4,26 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
-import android.view.MotionEvent
-import android.view.View.OnTouchListener
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.camerakit.CameraKitView
+import com.camerakit.CameraKitView.ImageCallback
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 
 
@@ -29,9 +32,13 @@ import java.io.IOException
  */
 class FirstFragment : Fragment() {
 
+    private lateinit var mediaPlayer: MediaPlayer
     private lateinit var mContext: Context
     private lateinit var mediaRecorder: MediaRecorder
     private var output: String? = null
+
+    private var cameraKitView: CameraKitView? = null
+
 
     override fun onAttach(context: Context) {
         super.onAttach(context);
@@ -46,14 +53,54 @@ class FirstFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+
+    override fun onResume() {
+        super.onResume()
+        cameraKitView?.onResume()
+    }
+
+    override fun onStart ()
+    {
+        super.onStart()
+        cameraKitView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cameraKitView?.onStop()
+    }
+
+    private fun scatta()
+    {
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        cameraKitView = view.findViewById(R.id.camera);
 
-        output = File(mContext.getExternalFilesDir("SiteSurvery"), "recording.mp3").toString()
+        output = File(mContext.getExternalFilesDir("SiteSurvey"), "tmp_recording.mp3").toString()
 
         Log.d("output",output)
         mediaRecorder = MediaRecorder()
+        mediaPlayer = MediaPlayer.create(mContext,R.raw.ding)
+
+        val shutter = view.findViewById<ImageButton>(R.id.shutter)
+        shutter.setOnClickListener{
+            Log.d("shutter","shu")
+            cameraKitView?.captureImage(ImageCallback { cameraKitView, capturedImage ->
+                val savedPhoto =File(mContext.getExternalFilesDir("SiteSurvey"), "tmp_photo.jpg")
+                Log.d("Photo",savedPhoto.toString())
+                try {
+                    val outputStream = FileOutputStream(savedPhoto.path)
+                    outputStream.write(capturedImage)
+                    outputStream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            })
+        }
 
 
 
@@ -74,6 +121,7 @@ class FirstFragment : Fragment() {
                         mediaRecorder?.setOutputFile(output)
                         mediaRecorder?.prepare()
                         mediaRecorder?.start()
+                        mediaPlayer.start()
                         Toast.makeText(mContext, "Recording started!", Toast.LENGTH_SHORT).show()
                         }
 
@@ -85,6 +133,7 @@ class FirstFragment : Fragment() {
                         mediaRecorder?.stop()
                         mediaRecorder?.release()
                         Toast.makeText(mContext, "Stop Recording", Toast.LENGTH_SHORT).show()
+                        mediaPlayer.start()
                     }catch (e: IllegalStateException) {
                         e.printStackTrace()
                     } catch (e: IOException) {
@@ -97,27 +146,7 @@ class FirstFragment : Fragment() {
             true
         }
 
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
-    private fun StartRecord() {
-        if (ContextCompat.checkSelfPermission(mContext,
-                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(mContext,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-            ActivityCompat.requestPermissions(mContext as Activity, permissions,0)
-        } else {
-            try {
-                mediaRecorder?.prepare()
-                mediaRecorder?.start()
-                Toast.makeText(mContext, "Recording started!", Toast.LENGTH_SHORT).show()
-            } catch (e: IllegalStateException) {
-                e.printStackTrace()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }        }
-    }
+
 }
--- app/src/main/java/com/luca/innocenti/sitesurveynotebook/MainActivity.kt
+++ app/src/main/java/com/luca/innocenti/sitesurveynotebook/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.luca.innocenti.sitesurveynotebook
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Environment
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
+    var posizione: Int =1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +24,34 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                sposta()
+            }
+        })
+    }
+
+    private fun sposta(){
+        // crea directory
+        Log.d("Directory","Sposta")
+        val sdf = SimpleDateFormat("dd_MM_yyyy_hh:mm:ss")
+        val currentDate = sdf.format(Date())
+        val folder_main = currentDate
+
+        val f =
+            File(this.getExternalFilesDir("SiteSurvey"), folder_main)
+        if (!f.exists()) {
+            f.mkdirs()
         }
+        var mp3_tmp:File = File(this.getExternalFilesDir("SiteSurvey"), "tmp_recording.mp3")
+        var jpg_tmp:File= File(this.getExternalFilesDir("SiteSurvey"), "tmp_photo.jpg")
+
+        var mp3_def: File = File(this.getExternalFilesDir("SiteSurvey/"+currentDate), currentDate+".mp3")
+        var jpg_def: File = File(this.getExternalFilesDir("SiteSurvey/"+currentDate), currentDate+".jpg")
+
+        mp3_tmp.copyTo(mp3_def,false)
+        jpg_tmp.copyTo(jpg_def,false)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,13 +60,25 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    // actions on click menu items
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.note -> {
+            if (posizione == 2) NavHostFragment.findNavController(nav_host_fragment).navigate(R.id.action_SecondFragment_to_FirstFragment)
+            posizione = 1
+            Log.d("Menu", "Note")
+            true
+        }
+        R.id.list -> {
+            Log.d("Menu", "Lista")
+            if (posizione == 1) NavHostFragment.findNavController(nav_host_fragment).navigate(R.id.action_FirstFragment_to_SecondFragment)
+            posizione = 2
+            true
+        }
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
         }
     }
+
 }
